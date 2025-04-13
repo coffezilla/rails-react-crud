0e13d07764a707bb3833638cdee1876f7d51b01e
Renato Santos <devrenato.js@gmail.com>
2025-04-13 16:13:21 -0300

    chore(tailwind) update to v4

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,11 +1,13 @@
 import { VitePWA } from "vite-plugin-pwa";
 
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		react(),
 		VitePWA({
 			registerType: "autoUpdate",
